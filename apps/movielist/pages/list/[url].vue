70f5038564f1fe250b7525e8846524294ea80543
Marwane El Kchiri <43742934+mxrwn@users.noreply.github.com>
2022-12-28 00:53:46 +0100

    Movie Api Setup

--- apps/movielist/pages/list/[url].vue
+++ apps/movielist/pages/list/[url].vue
@@ -1,40 +1,59 @@
 <script lang="ts" setup>
 const route = useRoute();
-
+const imageUrl = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/';
 const list = await $fetch(`/api/list/${route.params.url}`);
-
-console.log(list);
-
-async function creatingMovie() {
-  console.log('');
-}
+const movies = useData('chainsaw');
 </script>
 
 <template>
   <div class="list">
     <div class="list-header flex justify-between">
-      <h1 class="text-2xl font-bold">My Lists</h1>
-      <label for="my-modal-4" class="btn">Add movie</label>
-    </div>
-    <input id="my-modal-4" type="checkbox" class="modal-toggle" />
-    <label for="my-modal-4" class="modal cursor-pointer">
-      <label class="modal-box relative" for="">
-        <input
-          type="text"
-          placeholder="Search for a movie"
-          class="input w-full max-w-xs"
-        />
-        <div class="dropdown dropdown-open">
-          <label tabindex="0" class="btn m-1">Button</label>
-          <ul
-            tabindex="0"
-            class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 h-52"
-          >
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-          </ul>
+      <h1 v-if="list" class="text-2xl font-bold">{{ list.title }}</h1>
+      <div class="form-control">
+        <div class="input-group">
+          <input
+            type="text"
+            placeholder="Search…"
+            :value="text"
+            @input="(event) => searchingMovies(event)"
+            class="input input-bordered"
+          />
+          <button class="btn btn-square" type="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
         </div>
-      </label>
-    </label>
+      </div>
+    </div>
+    <div class="flex flex-wrap gap-10">
+      <div
+        v-for="media in movies"
+        :key="media.id"
+        class="card w-96 bg-base-100 shadow-xl"
+      >
+        <figure>
+          <img :src="`${imageUrl}/${media.poster_path}`" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">{{ media.original_title }}</h2>
+          <p>{{ media.overview }}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" type="button">Add Movie</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- apps/movielist/server/api/list/[url].ts
+++ apps/movielist/server/api/list/[url].ts
@@ -1,16 +1,10 @@
 import PrismaClient from 'db';
 
 export default defineEventHandler(async (event) => {
-  try {
-    const lists = await PrismaClient.list.findFirst({
-      where: {
-        url: event.context.params.url,
-      },
-    });
-    return lists?.id;
-  } catch (error: any) {
-    return {
-      error,
-    };
-  }
+  const lists = await PrismaClient.list.findFirst({
+    where: {
+      url: event.context.params.url,
+    },
+  });
+  return lists;
 });
